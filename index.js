7de1e83144b525b1ae0378b592c970203d8e2307
Brendon W <bwherry@midland.k12.ia.us>
2025-11-05 18:26:21 +0000

    4

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-function priceCheck (match){
-    if (match == 1.50) {
+function priceCheck (price){
+    if (price == 1.50) {
         return "match";
     } 
-
-    if (match == '1.50') {
-        return "match";
-    }
-
-     if (match == 1.00) {
-    }
 } 
 
 
-
+function strictPriceCheck (price) {
+    if (price === 1.50) {
+        return "match";
+    }
+}
 
 
 
